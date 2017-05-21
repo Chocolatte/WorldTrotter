1d75e0a453f49e14a056c49d0f99132114c8ad11
Zoltan <kubat.zoltan@gmail.com>
2017-05-21 18:49:29 +0200

    Commented out manual code.

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -13,16 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let firstFrame = CGRect(x:160, y:240, width:100, height: 150)
+//        let firstFrame = CGRect(x:160, y:240, width:100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x:20, y:30, width:50, height:50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        firstView.addSubview(secondView)
+//        
+//        let thirdFrame = CGRect(x:5, y:5, width:20, height:20)
+//        let thirdView = UIView(frame:thirdFrame)
+//        thirdView.backgroundColor = UIColor.orange
+//        secondView.addSubview(thirdView)
         
-        let firstView = UIView(frame: firstFrame)
-        
-        firstView.backgroundColor = UIColor.blue
-        
-        view.addSubview(firstViews)
         
         
-        }
+        
+        
+        
+        
+        
+        
+        
+        
+      }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
